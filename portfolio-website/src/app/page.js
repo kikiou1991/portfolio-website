99c38dd1e4c8baeb6c2943c955d54ae35f2f91a8
Gabor Adorjani <gadorjani@windowslive.com>
2023-09-07 22:05:32 +0100

    popup menu
    created pop-up menu for mobile devices...few more things to style but slowly making progress

--- portfolio-website/src/app/page.js
+++ portfolio-website/src/app/page.js
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#372E29] container mx-auto px-12 py-4">
--- portfolio-website/src/components/Navbar.jsx
+++ portfolio-website/src/components/Navbar.jsx
@@ -1,58 +1,81 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 import Link from 'next/link';
 import NavLink from './Navlink';
+import { Bars3Icon, XmarkIcon } from '@heroicons/react/24/solid';
+import MenuOverLay from './MenuOverLay';
 
 const navLinks = [
-    {
-        title: "About",
-        path: "#about"
-    },
-    {
-        title: "Portfolio",
-        path: "#portfolio"
-    },
-    {
-        title: "Contact",
-        path: "#contact"
-    }
-]
+  {
+    title: 'About',
+    path: '#about',
+  },
+  {
+    title: 'Portfolio',
+    path: '#portfolio',
+  },
+  {
+    title: 'Contact',
+    path: '#contact',
+  },
+];
 
 const Navbar = () => {
+  const [navBarOpen, setNavBarOpen] = useState(false);
+
   return (
     <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
       <div className='flex items-center justify-between mx-2 p-4 md:mx-auto px-4'>
         <div className='flex items-center'>
           <img
             src='/images/logo.png'
-            alt="Gabor the Wizard"
+            alt='Gabor the Wizard'
             width={50}
             height={50}
           />
         </div>
         <div className='ml-auto'>
-          <div className='md:hidden'>
-            <button
-              className='block text-white hover:text-white focus:text-white'
-              // Add functionality to open a mobile menu here
-            >
-              Menu
-            </button>
-          </div>
-          <div className='hidden md:flex items-center'>
-            <ul className='flex space-x-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
-              {
-                navLinks.map((link,index) => (
-                    <li key={index}>
-                        <NavLink href={link.path} title={link.title} />
-                    </li>
-                ))}
-        
+          <div className='menu hidden md:flex items-center'>
+            <ul className='flex space-x-4 md:p-0 md:flex-row md:space-x-10 mt-0'>
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink href={link.path} title={link.title} />
+                </li>
+              ))}
             </ul>
           </div>
         </div>
+        <div className='mobile-menu block md:hidden'>
+          {!navBarOpen ? (
+            <button
+              onClick={() => setNavBarOpen(true)}
+              className=' flex items-center
+                                        px-3 border rounded
+                                        border-slate-200
+                                        text-slate-200
+                                        hover:text-white
+                                        hover:border-white'
+            >
+              <Bars3Icon className='h-5 w-5' />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavBarOpen(false)}
+              className=' flex items-center
+                                        px-3 border rounded
+                                        border-slate-200
+                                        text-slate-200
+                                        hover:text-white
+                                        hover:border-white'
+            >
+              <XmarkIcon className='h-5 w-5' />
+            </button>
+          )}
+        </div>
       </div>
+      {navBarOpen ? <MenuOverLay links={navLinks} /> : null}
     </nav>
   );
-}
+};
 
 export default Navbar;
