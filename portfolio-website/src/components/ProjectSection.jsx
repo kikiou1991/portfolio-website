c6604de8ead6ec0cec94a7416cf6b013984ca4cc
Gabor Adorjani <gadorjani@windowslive.com>
2023-09-08 22:12:56 +0100

    styling cards
    playing around with some card styling

--- portfolio-website/src/components/ProjectSection.jsx
+++ portfolio-website/src/components/ProjectSection.jsx
@@ -59,7 +59,7 @@ const ProjectSection = () => {
             <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
                 My Projects
             </h2>
-            <div className='text-white flex flex-row justify-center text-center gap-2 mb-2 py-6'>
+            <div className='flex flex-row justify-center text-center text-white  gap-2 mb-2 py-6'>
                 {/* Project filter buttons */}
                 <ProjectTag onClick={handleTagChange} name="All" isSelected={tag === "All"} />
                 <ProjectTag onClick={handleTagChange} name="Web" isSelected={tag === "Web"} />
--- portfolio-website/src/components/ProjectsCard.jsx
+++ portfolio-website/src/components/ProjectsCard.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from 'react'
 import {CodeBracketIcon, EyeIcon} from '@heroicons/react/24/outline'
 import Link from 'next/link';
+import Image from 'next/image';
+
 
 
 const ProjectsCard = ({imgUrl, title, description, gitUrl}) => {
   return (
-    <div className='w-full md:w-[30%] h-full '>
-        <div className="h-52 md:h-72 rounded-t-xl relative group"
-             style={{ background: `url(${imgUrl})`, backgroundSize:"cover"}}>
+    <div className='w-full md:w-[30%] h-[400px]  '>
+        <div className="h-[70%] rounded-t-xl overflow-hidden relative group">
+            <Image src={imgUrl} fill style={{objectFit:'cover', width:'100%', height:'100%'}}/>
+            
 
-            <div className='overlay aspect-w-1 aspect-h-1 rounded-t-xl items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
+            <div className='overlay rounded-t-xl items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
                 <Link href={gitUrl} className='h-14 w-14 mr-2 border-2 relative rounded-full group/link border-white hover:border-[#FFD25F]'>
                     <CodeBracketIcon className='h-10 w-10 text-white group-hover/link:text-[#FFD25F] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer hover:white'></CodeBracketIcon>
                 /</Link>
@@ -20,7 +23,7 @@ const ProjectsCard = ({imgUrl, title, description, gitUrl}) => {
         
         </div>
 
-        <div className='text-white rounded-b-xl bg-[#181818] py-6 px-4 aspect-w-1 aspect-h-1'> 
+        <div className='text-white rounded-b-xl bg-[#181818] h-[30%] pt-2 pb-4 px-2'> 
             <h5 className='font-xl font-semibold mb-2'>{title}</h5>
             <p className='text-[#FFD25F]'>{description}</p>
         </div>
