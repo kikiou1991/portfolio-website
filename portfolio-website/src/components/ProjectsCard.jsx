4daf0d4ca2ee92c287ef1d673a40bee289c8aaea
kikiou91 <gadorjani@windowslive.com>
2024-01-22 18:26:32 +0000

    hover icon "/" correction
    removed / from inbetween the Link tags

--- portfolio-website/src/components/ProjectsCard.jsx
+++ portfolio-website/src/components/ProjectsCard.jsx
@@ -18,10 +18,10 @@ const ProjectsCard = ({imgUrl, title, description, gitUrl, imgAlt}) => {
             <div className='overlay rounded-t-xl gap-2 items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
                 <Link href="/" target="_blank" className='h-14 w-14 border-2 relative rounded-full group/link border-white hover:border-[#FFD25F]'>
                     <EyeIcon className='h-10 w-10 text-white group-hover/link:text-[#FFD25F] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer hover:white'></EyeIcon>
-                /</Link>
+                </Link>
                 <Link href={gitUrl} target="_blank" className='h-14 w-14 mr-2 border-2 relative rounded-full group/link border-white hover:border-[#FFD25F]'>
                     <CodeBracketIcon className='h-10 w-10 text-white group-hover/link:text-[#FFD25F] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer hover:white'></CodeBracketIcon>
-                /</Link>
+                </Link>
             </div>
         
         </div>
