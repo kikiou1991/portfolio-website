04a7799600d2b381af798c1585caacd3f773ef93
kikiou91 <gadorjani@windowslive.com>
2024-01-29 21:21:38 +0000

    Update Footer.jsx

--- portfolio-website/src/components/Footer.jsx
+++ portfolio-website/src/components/Footer.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+import { FaRegRegistered } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className='footer border border-top-[#33353F] border-l-transparent border-r-transparent'>
-        <div className='container p-12 flex justify-between text-slate-600'> 
-            <span>Logo</span>
-            <p>All rights reserved</p>
-        </div>
+      <div className='container p-12 flex justify-between text-white'>
+        <span>Gabor Adorjani</span>
+        <p className='flex items-center justify-center gap-2'>
+          All rights reserved <FaRegRegistered />
+        </p>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
