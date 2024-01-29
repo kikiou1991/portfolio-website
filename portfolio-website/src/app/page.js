67d40cefaca71024ee8251132782245eb5cc27bb
kikiou91 <gadorjani@windowslive.com>
2024-01-29 21:52:39 +0000

    navbar update 1.0.0

--- portfolio-website/src/app/page.js
+++ portfolio-website/src/app/page.js
@@ -5,11 +5,13 @@ import AboutSection from '@/components/AboutSection';
 import ProjectSection from '@/components/ProjectSection';
 import EmailSection from '@/components/EmailSection';
 import Footer from '@/components/Footer';
+import Navigation from '@/components/Navigation';
 
 export default function Home() {
   return (
     <main className='flex min-w-full min-h-screen flex-col bg-[#372E29] container mx-auto px-4 py-4'>
-      <Navbar />
+      {/* <Navbar /> */}
+      <Navigation />
       <div class='container mt-20 mx-auto items-center px-12 py-4'>
         <HeroSection />
         <AboutSection />
