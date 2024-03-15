0118f7650b282e3a9fc85a2e23e74becb5920dbe
kikiou91 <gadorjani@windowslive.com>
2024-03-15 16:16:41 +0000

    Update HeroSection.jsx

--- portfolio-website/src/components/HeroSection.jsx
+++ portfolio-website/src/components/HeroSection.jsx
@@ -51,7 +51,7 @@ const HeroSection = () => {
         >
           <h1
             className="text-white text-4xl sm:text-5xl lg:text-6xl font-extrabold"
-            style={{ minHeight: "180px" }}
+            style={{ minHeight: "160px" }}
           >
             {"Hello there,  "}
             <TypeAnimation
