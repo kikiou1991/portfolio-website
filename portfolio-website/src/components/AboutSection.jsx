748fe599803c531450af364df6d9512c75eb3cf8
kikiou91 <gadorjani@windowslive.com>
2024-03-15 15:47:16 +0000

    changes to text for hero section and About me

--- portfolio-website/src/components/AboutSection.jsx
+++ portfolio-website/src/components/AboutSection.jsx
@@ -1,15 +1,15 @@
-'use client';
-import React, { useRef, useState, useTransition } from 'react';
-import Image from 'next/image';
-import TabButton from './TabButton';
-import { motion } from 'framer-motion';
-import { useSectionInView } from '@/lib/custom-hooks/useSectionInView';
-import { TAB_DATA } from '@/lib/data/data';
+"use client";
+import React, { useRef, useState, useTransition } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/custom-hooks/useSectionInView";
+import { TAB_DATA } from "@/lib/data/data";
 
 const AboutSection = () => {
-  const [tab, setTab] = useState('skills');
+  const [tab, setTab] = useState("skills");
   const [isPending, startTransition] = useTransition();
-  const { ref } = useSectionInView('About');
+  const { ref } = useSectionInView("About");
 
   const handleTabChange = (id) => {
     startTransition(() => {
@@ -37,30 +37,53 @@ const AboutSection = () => {
       viewport={{
         once: true,
       }}
-      className='py-5 text-white my-5'
-      id='about'>
-      <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 h-full'>
-        <div className='relative w-full md:w-auto'>
-          <Image src='/images/study.png' layout='responsive' width={185} height={185} alt='study-office' />
+      className="py-5 text-white my-5"
+      id="about"
+    >
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 h-full">
+        <div className="relative w-full md:w-auto">
+          <Image
+            src="/images/study.png"
+            layout="responsive"
+            width={185}
+            height={185}
+            alt="study-office"
+          />
         </div>
-        <div className='mt-5 md:mt-0 text-left flex flex-col h-full'>
-          <h2 className='text-4xl font-bold text-[#EEBA35] mb-4'>About Me</h2>
-          <p className='text-base md:text-lg'>
-            I'm a dynamic web developer with expertise in JavaScript, HTML5, CSS3, GIT, React, and TailwindCSS. Committed to continuous growth, I'm actively pursuing full-stack engineering studies and
-            have completed projects like my Trello Clone. Eager to tackle new challenges and leave a lasting mark in web development. Ready to make things happen!
+        <div className="mt-5 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-[#EEBA35] mb-4">About Me</h2>
+          <p className="text-base md:text-lg">
+            I'm a dynamic web developer with expertise in JavaScript, Node.js,
+            React, MongoDB, GIT and TailwindCSS. Committed to continuous growth,
+            I'm actively pursuing full-stack engineering studies and have
+            completed projects that use my current stack, such as my version of
+            the popular Kanban style web app from Trello, called Wizardboards.
+            Eager to tackle new challenges and leave a lasting mark in web
+            development. Ready to make things happen!
           </p>
-          <div className='flex flex-row  justify-start mt-8'>
-            <TabButton selectTab={() => handleTabChange('skills')} active={tab === 'skills'}>
+          <div className="flex flex-row  justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
               Skills
             </TabButton>
-            <TabButton selectTab={() => handleTabChange('education')} active={tab === 'education'}>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
               Education
             </TabButton>
-            <TabButton selectTab={() => handleTabChange('experience')} active={tab === 'experience'}>
+            <TabButton
+              selectTab={() => handleTabChange("experience")}
+              active={tab === "experience"}
+            >
               Experience
             </TabButton>
           </div>
-          <div className='mt-8'>{TAB_DATA.find((t) => t.id === tab).content}</div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
         </div>
       </div>
     </motion.section>
--- portfolio-website/src/components/HeroSection.jsx
+++ portfolio-website/src/components/HeroSection.jsx
@@ -1,21 +1,21 @@
-'use client';
-import React from 'react';
-import { motion } from 'framer-motion';
-import Image from 'next/image';
-import { HiDownload } from 'react-icons/hi';
-import { TypeAnimation } from 'react-type-animation';
-import { useSectionInView } from '@/lib/custom-hooks/useSectionInView';
-import Link from 'next/link';
-import { useActiveSectionContext } from '@/context/active-section-context';
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { HiDownload } from "react-icons/hi";
+import { TypeAnimation } from "react-type-animation";
+import { useSectionInView } from "@/lib/custom-hooks/useSectionInView";
+import Link from "next/link";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 const HeroSection = () => {
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
-  const { ref } = useSectionInView('Home');
+  const { ref } = useSectionInView("Home");
 
   return (
-    <section ref={ref} className=''>
+    <section ref={ref} className="">
       <motion.div
-        className='grid grid-cols-1 sm:grid-cols-12 mt-10  mb-10'
+        className="grid grid-cols-1 sm:grid-cols-12 mt-10  mb-10"
         initial={{
           opacity: 0,
         }}
@@ -27,7 +27,8 @@ const HeroSection = () => {
         }}
         viewport={{
           once: true,
-        }}>
+        }}
+      >
         <motion.div
           initial={{
             opacity: 0,
@@ -46,49 +47,64 @@ const HeroSection = () => {
           viewport={{
             once: true,
           }}
-          className='col-span-7 place-self-center text-center sm:text-left'>
-          <h1 className='text-white text-4xl sm:text-5xl lg:text-6xl font-extrabold' style={{ minHeight: '180px' }}>
-            {'Hello there,  '}
+          className="col-span-7 place-self-center text-center sm:text-left"
+        >
+          <h1
+            className="text-white text-4xl sm:text-5xl lg:text-6xl font-extrabold"
+            style={{ minHeight: "180px" }}
+          >
+            {"Hello there,  "}
             <TypeAnimation
               sequence={[
                 // Same substring at the start will only be typed out once, initially
-                'I am Gabor',
+                "I am Gabor",
                 2000,
-                'and I am a web-dev',
+                "and I am a web-dev",
                 2000,
               ]}
-              wrapper='span'
+              wrapper="span"
               speed={50}
-              style={{ fontSize: '1em', display: 'inline-block' }}
+              style={{ fontSize: "1em", display: "inline-block" }}
               repeat={Infinity}
             />
           </h1>
-          <p className='text-[#EEBA35] text-base font-semibold sm:text-lg mb-6 lg:text-xl py-5'>
-            I am also a huge Harry Potter fan. That gave me the idea for the theme of this page, and also some of my favourite projects are centered around this as well. Thus mixing two of my
-            passions. I am passoinate and hardworking and I am always looking for new challenges.
+          <p className="text-[#EEBA35] text-base font-semibold sm:text-lg mb-6 lg:text-xl py-5">
+            Currently, my tech stack comprises React for front-end development,
+            Node.js for server-side scripting, and MongoDB for database
+            management, all stylized with Tailwind CSS for streamlined design.
+            Amidst my passion for technology, I've seamlessly blended my fervor
+            for the wizarding world of Harry Potter into various projects,
+            inspiring a unique fusion of my two passions. Always fueled by a
+            relentless pursuit of excellence, I thrive on embracing new
+            challenges and pushing the boundaries of innovation in both realms.
           </p>
           <div>
-            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
               <Link
                 onClick={() => {
-                  setActiveSection('Contact');
+                  setActiveSection("Contact");
                   setTimeOfLastClick(Date.now());
                 }}
-                href='#Contact'>
+                href="#Contact"
+              >
                 Hire me
               </Link>
             </button>
 
-            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4  bg-trasnparent hover:bg-slate-200 text-white border hover:text-black mt-3'>
-              <a href='/CV.pdf' download className='group flex flex-row gap-2 items center justify-center'>
-                Download CV{''}
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4  bg-trasnparent hover:bg-slate-200 text-white border hover:text-black mt-3">
+              <a
+                href="/CV.pdf"
+                download
+                className="group flex flex-row gap-2 items center justify-center"
+              >
+                Download CV{""}
                 <HiDownload
-                  className='
+                  className="
                   opacity-70 transition duration-200 ease-in-out
                   group-hover:translate-y-1
                   group-hover: -translate-x-1
                   justify-center items-center
-                '
+                "
                 />
               </a>
             </button>
@@ -112,9 +128,16 @@ const HeroSection = () => {
           viewport={{
             once: true,
           }}
-          className='col-span-5 mt-4 py-2 lg:mt-0 place-self-center text-center sm:text-left justify-center'>
-          <div className='rounded-[100%] bg-[#FFD25F] w-[265px] h-[265px] lg:w-[350px] lg:h-[350px] mx-auto relative justify-center items-center'>
-            <Image src='/images/wizard.png' alt='Gabor the Wizard' className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2' width={300} height={300} />
+          className="col-span-5 mt-4 py-2 lg:mt-0 place-self-center text-center sm:text-left justify-center"
+        >
+          <div className="rounded-[100%] bg-[#FFD25F] w-[265px] h-[265px] lg:w-[350px] lg:h-[350px] mx-auto relative justify-center items-center">
+            <Image
+              src="/images/wizard.png"
+              alt="Gabor the Wizard"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
           </div>
         </motion.div>
       </motion.div>
